51feb2f091ca9e4fce31b33a1d2ae55b30980bd6
Aviral Yadav <aviralx10@gmail.com>
2020-06-22 10:49:34 +0530

    Add files via upload

---
+++ build/precache-manifest.babd27292f1ed31ec6ca163b294e6bf6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da61f27e8e3a312ab1c3a59691cac551",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "77272d8f0ca91ce97f9b",
+    "url": "/static/css/main.8d46d287.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77272d8f0ca91ce97f9b",
+    "url": "/static/js/main.6904fd0c.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
